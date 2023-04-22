535a19093848d53330a3466f40c54e4270a4b2be
Anne Campelo <annaacampelo@gmail.com>
2023-04-22 19:55:35 +0200

    feat: adding chi, creating get, created 404 and 405 error handlers

--- cmd/Rammus.GG/main.go
+++ cmd/Rammus.GG/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe(":3000", r)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		w.Write([]byte("route does not exist"))
+	})
+
+	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(405)
+		w.Write([]byte("method is not valid"))
+	})
 }
